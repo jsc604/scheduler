3e50656cb5f39eadb8d059c322f7397bd375376b
Jeffrey Cheung <jeffreycheung_@live.com>
2023-01-11 14:47:00 -0800

    updated Form and added Header, Empty, Show to index as well as added Appointment to Application

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,13 +3,26 @@ import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
-  const [student, setStudent] = useState(props.student);
-  const [interviewer, setInterviewer] = useState(props.interviewer);
+  const [student, setStudent] = useState(props.student || '');
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent('');
+    setInterviewer(null);
+  };
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+  const save = () => {
+    props.onSave(student, interviewer);
+  };
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -27,8 +40,8 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={save}>Save</Button>
         </section>
       </section>
     </main>
