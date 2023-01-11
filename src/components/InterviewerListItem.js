ca7f12bbed3cc489304e5065ef951cf88e7f266c
Jeffrey Cheung <jeffreycheung_@live.com>
2023-01-10 16:40:00 -0800

    created InterviewerListItem component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+  const interviewersClass = classNames('interviewers__item', {
+    'interviewers__item--selected': props.selected
+  });
+
+  const interviewerName = props.selected ? props.name : '';
+
+  return (
+    <li
+      className={interviewersClass}
+      onClick={() => props.setInterviewer(props.id)}
+    >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={interviewerName}
+      />
+      {interviewerName}
+    </li>
+  );
+};
