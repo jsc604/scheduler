ea27159fd3bbca0d747ed800132b25c6bb7b5398
Jeffrey Cheung <jeffreycheung_@live.com>
2023-01-11 13:28:53 -0800

    created Form component

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -169,3 +170,19 @@ storiesOf("Appointment", module)
       message={'Could not delete appointment'}
       onClose={action('onClose')}
     />))
+  .add("Edit", () => (
+    <Form
+      student='Jeff'
+      interviewer={4}
+      interviewers={interviewers}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
+    />
+  ))
+  .add("Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action('onSave')}
+      onCancel={action('onCancel')}
+    />
+  ))
